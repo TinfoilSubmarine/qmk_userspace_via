8465a3b5ba8ed68fb71f32fd70c96d7b6993655b
Joel Beckmeyer <joel@beckmeyer.us>
2025-02-19 12:08:20 -0500

    add janky Alt+Tab/Space code

--- keyboards/ibm/model_m/modelh/keymaps/via/keymap.c
+++ keyboards/ibm/model_m/modelh/keymaps/via/keymap.c
@@ -15,6 +15,34 @@
  */
 #include QMK_KEYBOARD_H
 
+bool is_lgui_pressed = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_LGUI:
+      is_lgui_pressed = record->event.pressed;
+      if (!is_lgui_pressed) {
+        unregister_code(KC_LALT);
+      }
+      break;
+    case KC_TAB:
+      if (record->event.pressed && is_lgui_pressed) {
+        dprint("entering emulated Alt+Tab mode...\n");
+        register_code(KC_LALT);
+        unregister_code(KC_LGUI);
+      }
+      break;
+    case KC_SPC:
+      if (record->event.pressed && is_lgui_pressed) {
+        dprint("entering emulated Alt+GUI mode...\n");
+        register_code(KC_LALT);
+        return false;
+      }
+      break;
+  }
+  return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_fullsize_ansi_wkl( /* Base layer */
     KC_ESC,           KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_PSCR, KC_SCRL, KC_PAUS,
